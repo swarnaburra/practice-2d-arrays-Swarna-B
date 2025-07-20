f87478e497d0a0f5a4057826e4067ff00db2efd9
swarnaburra <swarnaburra2025@gmail.com>
2025-07-20 01:46:30 -0500

    Adding 2d arrays

---
+++ 2dArrays.js
@@ -0,0 +1,17 @@
+
+let allScores = [
+    [67, 92, 83],
+    [53, 89, 74],
+    [97, 71, 42]
+]
+
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"], 
+    ["B1", "B2", "B3", "B4"], 
+    ["C1", "C2", "C3", "C4"]  
+]
+
+let contactList = [
+    ["William", "william.kendall@xyz.com", "567-234-7681"],
+    ["Guru", "Guru.r@abc.com", "783-678-5678"]
+]
