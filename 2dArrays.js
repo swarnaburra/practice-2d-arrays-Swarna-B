27afa27a963161ba6d97b50f2fe551db2a5c9359
swarnaburra <swarnaburra2025@gmail.com>
2025-07-20 23:21:03 -0500

    adding console.log

--- 2dArrays.js
+++ 2dArrays.js
@@ -5,13 +5,19 @@ let allScores = [
     [97, 71, 42]
 ]
 
+console.log(allScores);
+
 let theaterSeating = [
     ["A1", "A2", "A3", "A4"], 
     ["B1", "B2", "B3", "B4"], 
     ["C1", "C2", "C3", "C4"]  
 ]
 
+console.log(theaterSeating);
+
 let contactList = [
     ["William", "william.kendall@xyz.com", "567-234-7681"],
     ["Guru", "Guru.r@abc.com", "783-678-5678"]
 ]
+
+console.log(contactList);
